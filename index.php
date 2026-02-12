9034ff46be65f0348add3260eb14b75e9b508d44
Aluko Olamide <olaluko20@gmail.com>
2026-02-12 05:45:27 -0800

    Create index.php
    main html and php logic added

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <label for="">First Number</label>
+        <input name= "firstNumber"  type="number">
+        <label for="secondNumber">Second Number</label>
+        <input name= "secondNumber" type="number">
+        <select name="operator" id="">
+            <option value="+">Additon</option>
+             <option value="-">Subtraction</option>
+              <option value="*">Multiplication</option>
+               <option value="/">Division</option>
+        </select>
+        <br>
+
+        <button type="submit">Submit</button>
+
+
+
+    </form>
+    <?php  
+    if ($_SERVER["REQUEST_METHOD"]== 'POST'){
+
+        //sanitize input
+        $firstNumber= filter_input(INPUT_POST,'firstNumber', FILTER_SANITIZE_NUMBER_FLOAT);
+         $secondNumber= filter_input(INPUT_POST,'secondNumber', FILTER_SANITIZE_NUMBER_FLOAT);
+          $operator= htmlspecialchars($_POST['operator']); 
+          $errors=false;
+
+          //check if empty input exists
+
+          if ( empty($firstNumber)||empty($secondNumber)||empty($operator)){
+            echo "<p> please fill all fields</p>";
+            $errors= true;
+          }
+
+          //check if empty input is not numeric
+
+
+          if ( !is_numeric($firstNumber)||!is_numeric($secondNumber)){
+            echo "<p> Only numbers please</p>";
+            $errors= true;
+          }
+
+
+          //check operator
+
+
+
+          if (!$errors){
+            $value= 0;
+          
+            switch($operator){
+                
+                case "+":
+                    $value= $firstNumber + $secondNumber;
+                    break;
+                case "-":
+                    $value= $firstNumber - $secondNumber;
+                    break;
+                case "*":
+                    $value= $firstNumber * $secondNumber;
+                    break;
+                case "/":
+                    $value= $firstNumber / $secondNumber;
+                    break;
+                default:
+                echo "<p> something went wrong </p>";
+                    
+                    break;                
+
+
+            }
+            //outut calculation
+            echo "<p> Result = ". $value ."</p>";
+
+
+
+          }
+
+    }
+    ?>
+</body>
+</html>
